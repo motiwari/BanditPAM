361df70d73fc2aa19100db433aa19a5147bf732c
James Mayclin <29185101+jmayclin@users.noreply.github.com>
2020-06-09 18:23:27 -0500

    documentation update

--- README.md
+++ README.md
@@ -1,14 +1,57 @@
 # cooking_spray
 C++ implmentation of PAM with confidence bounds.
 
-installation
-from main directory
+## installation
+### requirements
+Building this repository requires three external requirements: 
+Cmake >= 3.17
+Armadillo >= 9.7
+openMP
+
+Armadillo is a C++ library for linear algebra and matrix operations, and OpenMP is 
+a language/package used for multithreading.
+
+Armadillo: http://arma.sourceforge.net/download.html
+OpenMP: https://www.openmp.org/resources/openmp-compilers-tools/
+OpenMP is supported by default on most Linux platforms, and can be downloaded through
+homebrew on macs.
+
+### building
+The build process is automated with cmake. After cloning the repository the program
+can be built with the following steps.
 ```
-cd build
-cmake ..
-make
+~/cooking_spray$ mkdir build
+~/cooking_spray$ cd build
+~/cooking_spray/build$ cmake ..
+~/cooking_spray/build$ make
 ```
 
-for command line arguments
-`./pam ../data/mnist.csv output_name 5`
-will run the pam algorithim on the mnist toy set with 5 clusters. The output format is not currently implemented (but will just involve writing assignments and medoids to a file)
+## usage
+The easiest way to use the program is with the compiled `pam` binary. This is a command 
+line program that takes in three arguments: the dataset, the output path, and the number 
+of clusters to fit the data to.
+
+To run the KMedoids algorithim on the MNIST dataset that is contained in a file called 
+`mnist.csv` contained in a `data` folder, and we wish to fit our data to 5 clusters then
+we could run the following command.
+```
+~/cooking_spray/build$ ./pam ../data/mnist.csv output_name 5
+```
+
+## future development
+* Things to fix
+* should data be a parameter that is passed into the constructor?
+* medoid_indices should be private class variable?
+* does the medoid matrix actually help things?
+* To just consider n - k for swaps, just change candidate vector?
+* generic matrix type
+* generic metric type
+* error checking for number of clusters >= unique data points
+* should the mediods be index numbers or the mediods themselves?
+* switch all naming to camelCase
+* switch to armadillo typedefs
+* switch to rowvec
+* most typedefs are 64 bits, overkill?
+* check if N in build targets, then just iterate?
+* should loss function be a template object?
+* change name so can work with basic and ucb variation
--- headers/kmedoids_ucb.hpp
+++ headers/kmedoids_ucb.hpp
@@ -1,13 +1,14 @@
-/*
- * Things to fix
- * generic matrix type
- * generic metric type
- * error checking for number of clusters >= unique data points
- * should the mediods be index numbers or the mediods themselves?
+/**
+ * @file kmedoids_ucb.hpp
+ * @author james mayclin (jam.mayc@sgmail.com)
+ * @brief 
+ * @version 1.0
+ * @date 2020-06-08
+ * 
+ * This header file specifies the implementation of a KMedoids algorithm
+ * that makes use of theory specified in a paper submitted to NeurIPS
  */
 
-#ifndef MLPACK_METHODS_KMEANS_KMEANS_HPP
-#define MLPACK_METHODS_KMEANS_KMEANS_HPP
 
 #include <math.h>
 #include <iostream>
@@ -15,21 +16,48 @@
 #include <omp.h>
 #include <fstream>
 
-// switch all naming to camelCase
-// switch to armadillo typedefs
-// switch to rowvec
-// most typedefs are 64 bits, overkill?
 class KMediods
 {
 public:
+    /**
+     * @brief Construct a new KMediods object
+     * 
+     * @param maxIterations the maximum number of iterations for the swap step
+     */
     KMediods(size_t maxIterations = 1000);
 
+    /**
+     * @brief main method for kmedoids algorithm. Builds initial medoids then
+     * calls the swap method and iterates until convergence.
+     * 
+     * @param data column major dataset that the kmedoids algorithm will be run on
+     * @param clusters number of clusters to fit data to
+     * @param assignments vector which indicates the cluster that data point i belonds to
+     * @param medoid_indicies vector where indices(i) is the medoid of cluster i
+     */
     void cluster(const arma::mat &data,
                  const size_t clusters,
                  arma::urowvec &assignments,
                  arma::urowvec &medoid_indicies);
 
 private:
+    /**
+     * @brief build the sigma vector for the build step
+     * 
+     * This method is called before a medoid is built, and calculates
+     * the standard deviation of the loss selected from a number of 
+     * temporary reference points. On the first iteration the absolute 
+     * loss is used, and on all subsequent iterations the difference in loss 
+     * is used.
+     * 
+     * @param data column major dataset that the kmedoids algorithm will be run on
+     * @param best_distances vector containing the minimum distance between point i
+     * and a medoid
+     * @param sigma vector containing the sigmas for setting point i to be the next medoid
+     * @param batch_size batch size used in calcuating sigma
+     * @param use_absolute boolean valute indicating whether to use absolute loss or difference
+     * of loss. Set to true on first iteration, false after.
+     */
     void build_sigma(
         const arma::mat &data,
         arma::rowvec &best_distances,
@@ -37,6 +65,21 @@ private:
         arma::uword batch_size,
         bool use_absolute);
 
+    /**
+     * @brief calculate an estimation of loss for targets
+     * 
+     * This method calculates an estimate for the loss of the points specified in target
+     * using batch_size number of points.
+     * 
+     * @param data column major dataset that the kmedoids algorithim will be run on
+     * @param target points to calculate an estimation of loss for
+     * @param batch_size number of points to use in calculating estimate of loss
+     * @param best_distances vector containing the minimum distance between point i
+     * and a medoid
+     * @param use_absolute boolean valute indicating whether to use absolute loss or difference
+     * of loss. Set to true on first iteration, false after.
+     * @return arma::rowvec containing the estimates of the loss for the target points
+     */
     arma::rowvec build_target(
         const arma::mat &data,
         arma::uvec &target,
@@ -44,6 +87,14 @@ private:
         arma::rowvec &best_distances,
         bool use_absolute);
 
+    /**
+     * @brief Select the initial medoids using a greedy initialization
+     * 
+     * @param data column major dataset that the kmedoids algorithim will be run on
+     * @param clusters number of clusters to select
+     * @param medoid_indicies indicies in data of the selected medoids
+     * @param medoids matrix containing the selected medoids in column major format
+     */
     void build(const arma::mat &data,
                const size_t clusters,
                arma::urowvec &medoid_indicies,
@@ -51,6 +102,18 @@ private:
 
     double cost_fn_build(const arma::mat &data, arma::uword target, arma::uvec &tmp_refs, arma::rowvec &best_distances);
 
+    /**
+     * @brief calculate estimates of the difference in loss for targets in the swap step
+     * 
+     * @param data column major dataset that the kmedoids algorithim will be run on
+     * @param medoid_indices indices of medoids
+     * @param targets targets to calculate estimate of loss difference for
+     * @param batch_size number of reference points to use to estimate loss
+     * @param best_distances minimum distance between point i and a medoid
+     * @param second_best_distances second smallest distance between point i and a medoid
+     * @param assignments vector containing the current cluster assignments for each data point
+     * @return arma::vec containing the estimates of loss difference for the specified targets.
+     */
     arma::vec swap_target(
         const arma::mat &data,
         arma::urowvec &medoid_indices,
@@ -60,15 +123,41 @@ private:
         arma::rowvec &second_best_distances,
         arma::urowvec &assignments);
 
+    /**
+     * @brief iteratively swap medoids until convergence or maxIterations
+     * 
+     * @param data column major dataset that the kmedoids algorithim will be run on
+     * @param clusters number of clusters to fit data to
+     * @param medoid_indicies indiceies of data points that are serving as medoids
+     * @param medoids column major matrix of medoids
+     */
     void swap(const arma::mat &data,
               const size_t clusters,
               arma::urowvec &medoid_indicies,
               arma::mat &medoids);
 
+    /**
+     * @brief debugging function for checking absolute loss of some given medoid_indices
+     * 
+     * @param data column major dataset
+     * @param clusters number of clusters to use
+     * @param medoid_indicies vector containing indicies of medoids
+     * @return double indicating total loss
+     */
     double calc_loss(const arma::mat &data,
                      const size_t clusters,
                      arma::urowvec &medoid_indicies);
 
+    /**
+     * @brief calculate sigma for swap iteration
+     * 
+     * @param data column major dataset
+     * @param sigma vector of std deviation of loss difference for each iteration
+     * @param batch_size number of reference points to use in estimate of loss difference
+     * @param best_distances minimum distance between a point and and medoid
+     * @param second_best_distances second best distance between a point and any medoid
+     * @param assignments vector containing the current cluster assignments for each data point
+     */
     void swap_sigma(
         const arma::mat &data,
         arma::mat &sigma,
@@ -81,5 +170,3 @@ private:
     size_t maxIterations;
     int verbosity = 0;
 };
-
-#endif
