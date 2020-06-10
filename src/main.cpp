f0a6d4d9ae20d7fb72de0473606164f354f8b134
James Mayclin <29185101+jmayclin@users.noreply.github.com>
2020-06-09 23:51:02 -0500

    removed extraneous comment from main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 #include "kmedoids_ucb.hpp"
 #include <armadillo>
 #include <chrono>
-using namespace arma;
+#include <fstream>
+
 int main(int argc, char *argv[]) {
-
-    // how often are things resampled?
-    // standardize the typedefs
-    // choose between rowvec and vec
-
+    if (argc < 3 || argc > 4) {
+        std::cout << "incorrect usage" << std::endl;
+        return 1;
+    }
     std::string input_name = argv[1];
     std::string output_name = argv[2];
     int k = std::stoi(argv[3]);
 
     arma::mat data;
-    // data.load("../test_cases/data1.csv", arma::csv_ascii);
-    // data.load("../data/mnist.csv", arma::csv_ascii);
     data.load(input_name);
     data = arma::trans(data);
+
     arma::uword n = data.n_cols;
     arma::uword d = data.n_rows;
-    std::cout << "Read in " << n << " data points of dimension " << d
-              << std::endl;
+    std::cout << "Read in " 
+        << n 
+        << " data points of dimension " 
+        << d
+        << std::endl;
 
     arma::urowvec assignments(n);
     arma::urowvec medoid_indices(k);
@@ -34,5 +36,4 @@ int main(int argc, char *argv[]) {
                 .count()
          << " milliseconds" << endl;
 
-    // data.load('data1.csv', csv_ascii);
 }
