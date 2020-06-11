c4bdfc6b5075d8d1ab2034c1bcc50af3f1ecc128
James Mayclin <29185101+jmayclin@users.noreply.github.com>
2020-06-10 22:44:33 -0500

    removed dead comments, refactored to avoid akward formatting. Closes #17

--- src/kmediods_naive.cpp
+++ src/kmediods_naive.cpp
@@ -10,14 +10,7 @@ KMediods::cluster(const arma::mat& data,
                   const size_t clusters,
                   arma::Row<size_t>& assignments)
 {
-    // arma::mat centroids(data.n_rows, clusters);
     arma::Row<size_t> centroid_indices(clusters);
-    // the thought is that having centroids as its own structure would
-    // minimize cache misses. But it is also necessary to have the index
-    // of the centroids.
-    //
-    // Should the swap be happening with the updated clusters? yes?
-
     // build clusters
     KMediods::build(data, clusters, centroid_indices);
     std::cout << centroid_indices << std::endl;
@@ -29,11 +22,9 @@ KMediods::cluster(const arma::mat& data,
         KMediods::swap(data, clusters, assignments, centroid_indices);
         std::cout << centroid_indices << std::endl;
         medoid_change = arma::any(centroid_indices != previous);
-        // medoid_change = true;
         std::cout << "mediod change is " << medoid_change << std::endl;
         i++;
     }
-    // swap thingy
 }
 
 void
@@ -44,18 +35,14 @@ KMediods::build(const arma::mat& data,
     for (size_t k = 0; k < clusters; k++) {
         double minDistance = std::numeric_limits<double>::infinity();
         size_t best = 0;
-        for (size_t i = 0; i < data.n_cols;
-             i++) { // consider each data point as a potentail mediod
+        for (size_t i = 0; i < data.n_cols; i++) {
             double total = 0;
             for (size_t j = 0; j < data.n_cols; j++) {
-                double cost = arma::norm(data.col(i) - data.col(j),
-                                         2); // either add distance between
-                                             // potential mediod and point
-                for (size_t mediod = 0; mediod < k;
-                     mediod++) { // or closer mediod and point
-                    if (arma::norm(data.col(centroid_indices(mediod)) -
-                                     data.col(j),
-                                   2) < cost) {
+                double cost = arma::norm(data.col(i) - data.col(j), 2);
+                for (size_t mediod = 0; mediod < k; mediod++) {
+                    if (arma::norm(
+                          data.col(centroid_indices(mediod)) - data.col(j), 2) <
+                        cost) {
                         cost = arma::norm(
                           data.col(centroid_indices(mediod)) - data.col(j), 2);
                     }
@@ -82,24 +69,17 @@ KMediods::swap(const arma::mat& data,
     size_t best = 0;
     size_t medoid_to_swap = 0;
     for (size_t k = 0; k < clusters; k++) {
-
-        for (size_t i = 0; i < data.n_cols;
-             i++) { // consider each data point as a potential mediod
+        for (size_t i = 0; i < data.n_cols; i++) {
             double total = 0;
             for (size_t j = 0; j < data.n_cols; j++) {
-                double cost = arma::norm(data.col(i) - data.col(j),
-                                         2); // either add distance between
-                                             // potential mediod and point
-                for (size_t mediod = 0; mediod < clusters;
-                     mediod++) { // or closer mediod and point
+                double cost = arma::norm(data.col(i) - data.col(j), 2);
+                for (size_t mediod = 0; mediod < clusters; mediod++) {
                     if (mediod == k) {
                         continue;
                     }
-                    if (arma::norm(data.col(centroid_indices(mediod)) -
-                                     data.col(j),
-                                   2) < cost) {
-                        cost = arma::norm(
-                          data.col(centroid_indices(mediod)) - data.col(j), 2);
+                    double current = arma::norm(data.col(centroid_indices(mediod)) - data.col(j), 2);
+                    if (current < cost) {
+                        cost = current;
                     }
                 }
                 total += cost;
@@ -121,14 +101,13 @@ KMediods::calc_loss(const arma::mat& data,
                     arma::Row<size_t>& centroid_indices)
 {
     double total = 0;
-
     for (size_t i = 0; i < data.n_cols; i++) {
         double cost = std::numeric_limits<double>::infinity();
         for (size_t mediod = 0; mediod < clusters; mediod++) {
-            if (arma::norm(data.col(centroid_indices(mediod)) - data.col(i),
-                           2) < cost) {
-                cost = arma::norm(
-                  data.col(centroid_indices(mediod)) - data.col(i), 2);
+            double current =
+              arma::norm(data.col(centroid_indices(mediod)) - data.col(i), 2);
+            if (current < cost) {
+                cost = current;
             }
         }
         total += cost;
