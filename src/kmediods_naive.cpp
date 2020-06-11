d784a4bcf0fadf31745fc351ea77662c5c839870
James Mayclin <29185101+jmayclin@users.noreply.github.com>
2020-06-10 22:45:53 -0500

    forot formatting issue with norm

--- src/kmediods_naive.cpp
+++ src/kmediods_naive.cpp
@@ -10,19 +10,19 @@ KMediods::cluster(const arma::mat& data,
                   const size_t clusters,
                   arma::Row<size_t>& assignments)
 {
-    arma::Row<size_t> centroid_indices(clusters);
+    arma::Row<size_t> centroidIndices(clusters);
     // build clusters
-    KMediods::build(data, clusters, centroid_indices);
-    std::cout << centroid_indices << std::endl;
+    KMediods::build(data, clusters, centroidIndices);
+    std::cout << centroidIndices << std::endl;
 
     size_t i = 0;
-    bool medoid_change = true;
-    while (i < this->maxIterations && medoid_change) {
-        auto previous(centroid_indices);
-        KMediods::swap(data, clusters, assignments, centroid_indices);
-        std::cout << centroid_indices << std::endl;
-        medoid_change = arma::any(centroid_indices != previous);
-        std::cout << "mediod change is " << medoid_change << std::endl;
+    bool medoidChange = true;
+    while (i < this->maxIterations && medoidChange) {
+        auto previous(centroidIndices);
+        KMediods::swap(data, clusters, assignments, centroidIndices);
+        std::cout << centroidIndices << std::endl;
+        medoidChange = arma::any(centroidIndices != previous);
+        std::cout << "mediod change is " << medoidChange << std::endl;
         i++;
     }
 }
@@ -30,7 +30,7 @@ KMediods::cluster(const arma::mat& data,
 void
 KMediods::build(const arma::mat& data,
                 const size_t clusters,
-                arma::Row<size_t>& centroid_indices)
+                arma::Row<size_t>& centroidIndices)
 {
     for (size_t k = 0; k < clusters; k++) {
         double minDistance = std::numeric_limits<double>::infinity();
@@ -40,11 +40,10 @@ KMediods::build(const arma::mat& data,
             for (size_t j = 0; j < data.n_cols; j++) {
                 double cost = arma::norm(data.col(i) - data.col(j), 2);
                 for (size_t mediod = 0; mediod < k; mediod++) {
-                    if (arma::norm(
-                          data.col(centroid_indices(mediod)) - data.col(j), 2) <
-                        cost) {
-                        cost = arma::norm(
-                          data.col(centroid_indices(mediod)) - data.col(j), 2);
+                    double current = arma::norm(
+                      data.col(centroidIndices(mediod)) - data.col(j), 2);
+                    if (current < cost) {
+                        cost = current;
                     }
                 }
                 total += cost;
@@ -55,7 +54,7 @@ KMediods::build(const arma::mat& data,
             }
         }
         std::cout << best << " and " << minDistance << std::endl;
-        centroid_indices(k) = best;
+        centroidIndices(k) = best;
     }
 }
 
@@ -63,7 +62,7 @@ void
 KMediods::swap(const arma::mat& data,
                const size_t clusters,
                arma::Row<size_t>& assignments,
-               arma::Row<size_t>& centroid_indices)
+               arma::Row<size_t>& centroidIndices)
 {
     double minDistance = std::numeric_limits<double>::infinity();
     size_t best = 0;
@@ -77,7 +76,8 @@ KMediods::swap(const arma::mat& data,
                     if (mediod == k) {
                         continue;
                     }
-                    double current = arma::norm(data.col(centroid_indices(mediod)) - data.col(j), 2);
+                    double current = arma::norm(
+                      data.col(centroidIndices(mediod)) - data.col(j), 2);
                     if (current < cost) {
                         cost = current;
                     }
@@ -92,20 +92,20 @@ KMediods::swap(const arma::mat& data,
         }
     }
     std::cout << best << " mindistance is now" << minDistance << std::endl;
-    centroid_indices(medoid_to_swap) = best;
+    centroidIndices(medoid_to_swap) = best;
 }
 
 double
 KMediods::calc_loss(const arma::mat& data,
                     const size_t clusters,
-                    arma::Row<size_t>& centroid_indices)
+                    arma::Row<size_t>& centroidIndices)
 {
     double total = 0;
     for (size_t i = 0; i < data.n_cols; i++) {
         double cost = std::numeric_limits<double>::infinity();
         for (size_t mediod = 0; mediod < clusters; mediod++) {
             double current =
-              arma::norm(data.col(centroid_indices(mediod)) - data.col(i), 2);
+              arma::norm(data.col(centroidIndices(mediod)) - data.col(i), 2);
             if (current < cost) {
                 cost = current;
             }
