fd79233e5197a4ec82a30c1b8498e9224b55e2d2
esfrankel <esfrankel17@gmail.com>
2020-10-12 14:32:07 -0700

    #38

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,17 @@
+######### Helper Functions #########
+function download {
+    url=$1
+    filename=$2
+
+    if [ -x "$(which wget)" ] ; then
+        wget -q $url -O $2
+    elif [ -x "$(which curl)" ]; then
+        curl -o $2 -sfL $url
+    else
+        echo "Could not find curl or wget, please install one." >&2
+    fi
+}
+
 ######### Executables #########
 chmod +x scripts/*
 chmod +x build_docker.sh
@@ -5,10 +19,11 @@ chmod +x build_docker.sh
 ######### Get data #########
 mkdir -p data
 #### Get SCRNA data
-wget -P data http://web.stanford.edu/~ericsf/scrna_reformat.csv.gz
+download http://web.stanford.edu/~ericsf/scrna_reformat.csv.gz ./data/scrna_reformat.csv.gz
+
 #### Get MNIST data
-wget -P data http://web.stanford.edu/~ericsf/mnist.csv
-wget -P data http://web.stanford.edu/~ericsf/MNIST-70k.csv
+download http://web.stanford.edu/~ericsf/mnist.csv ./data/MNIST.csv
+download http://web.stanford.edu/~ericsf/MNIST-70k.csv ./data/MNIST-70k.csv
 head -60000 data/MNIST-70k.csv > data/MNIST-60k.csv
 head -65000 data/MNIST-70k.csv > data/MNIST-65k.csv
 head -10000 data/MNIST-70k.csv > data/MNIST-10k.csv
