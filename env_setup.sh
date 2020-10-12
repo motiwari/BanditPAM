a43a46ea30a8e67a859ea9a4f4d731949568c8a9
esfrankel <esfrankel17@gmail.com>
2020-10-12 14:46:31 -0700

    removing downlads, need to fix C++ tests #38

--- env_setup.sh
+++ env_setup.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 ######### Helper Functions #########
-function download {
-    url=$1
-    filename=$2
+download () {
+  url=$1
+  filename=$2
 
-    if [ -x "$(which wget)" ] ; then
-        wget -q $url -O $2
-    elif [ -x "$(which curl)" ]; then
-        curl -o $2 -sfL $url
-    else
-        echo "Could not find curl or wget, please install one." >&2
-    fi
+  # if [ -x "$(which wget)" ] ; then
+  #     wget -O $filename $url
+  if [ -x "$(which curl)" ]; then
+      curl -o $filename -sfL $url -v
+  else
+      echo "Could not find curl or wget, please install one." >&2
+  fi
 }
 
 ######### Executables #########
@@ -24,12 +25,6 @@ download http://web.stanford.edu/~ericsf/scrna_reformat.csv.gz ./data/scrna_refo
 #### Get MNIST data
 download http://web.stanford.edu/~ericsf/mnist.csv ./data/MNIST.csv
 download http://web.stanford.edu/~ericsf/MNIST-70k.csv ./data/MNIST-70k.csv
-head -60000 data/MNIST-70k.csv > data/MNIST-60k.csv
-head -65000 data/MNIST-70k.csv > data/MNIST-65k.csv
-head -10000 data/MNIST-70k.csv > data/MNIST-10k.csv
-head -20000 data/MNIST-70k.csv > data/MNIST-20k.csv
-head -40000 data/MNIST-70k.csv > data/MNIST-40k.csv
-head -1000 data/MNIST-70k.csv > data/MNIST-1k.csv
 
 ######### Run scripts #########
 sh ./build_docker.sh
