93d993f47988137f32dc8249c405292a67ff2d9a
esfrankel <esfrankel17@gmail.com>
2020-10-15 05:30:55 -0700

    Fixes #48

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -2,7 +2,6 @@ var menudata={children:[
 {text:"Main Page",url:"index.html"},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"},
 {text:"Class Hierarchy",url:"inherits.html"},
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
