e9d73719761d920c84ee8f3f514c4ec376de53f0
esfrankel <esfrankel17@gmail.com>
2020-10-15 10:02:31 -0700

    fixes #33

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -6,5 +6,5 @@ var searchData=
   ['build_5fsigma',['build_sigma',['../classKMedoids.html#aa655c4a1d55d8d49be343f3d6d62a0c2',1,'KMedoids']]],
   ['build_5ftarget',['build_target',['../classKMedoids.html#ab9734e538771e3a25b48c1a2a1b9f65c',1,'KMedoids']]],
   ['buildconfidence',['buildConfidence',['../classKMedoids.html#a75a5dc3ec625069a5fc98d133b2c22df',1,'KMedoids']]],
-  ['banditpam_3a_20almost_20linear_2dtime_20k_2dmedoids_20clustering',['BanditPAM: Almost Linear-Time k-Medoids Clustering',['../index.html',1,'']]]
+  ['banditpam_3a_20almost_20linear_2dtime_20_2ak_2a_2dmedoids_20clustering',['BanditPAM: Almost Linear-Time *k*-Medoids Clustering',['../index.html',1,'']]]
 ];
--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['banditpam_3a_20almost_20linear_2dtime_20k_2dmedoids_20clustering',['BanditPAM: Almost Linear-Time k-Medoids Clustering',['../index.html',1,'']]]
+  ['banditpam_3a_20almost_20linear_2dtime_20_2ak_2a_2dmedoids_20clustering',['BanditPAM: Almost Linear-Time *k*-Medoids Clustering',['../index.html',1,'']]]
 ];
