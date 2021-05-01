d18e8c2263cffd5e4d4486b45ebc00d603f2b81d
Ben-H3 <benhora@icloud.com>
2021-04-30 23:51:23 -0700

    Logfile Parameter In CPP Binary

--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,22 @@
 int main(int argc, char* argv[])
 {
     std::string input_name;
+    std::string log_file_name = "KMedoidsLogfile";
     int k;
     int opt;
     int verbosity = 0;
+    int max_iter = 1000;
     std::string loss = "2";
 
-    while ((opt = getopt(argc, argv, "f:l:k:v:")) != -1) {
+    while ((opt = getopt(argc, argv, "f:n:l:k:v:")) != -1) {
         switch (opt) {
             // path to the data file to be read in
             case 'f':
                 input_name = optarg;
+                break;
+            // path to log output data
+            case 'n':
+                log_file_name = optarg;
                 break;
             // number of clusters to create
             case 'k':
@@ -37,14 +43,14 @@ int main(int argc, char* argv[])
             // type of loss/distance function to use
             case 'l':
                 loss = optarg;
-		if (loss.at(0) == 'L') {
-			loss = loss.substr(1);
-		}
-		break;
+                if (loss.at(0) == 'L') {
+                  loss = loss.substr(1);
+                }
+                break;
             // set the verbosity of the algorithm
             case 'v':
-            	verbosity = std::stoi(optarg);
-            	break;
+                verbosity = std::stoi(optarg);
+                break;
             case ':':
                 printf("option needs a value\n");
                 return 1;
@@ -58,7 +64,7 @@ int main(int argc, char* argv[])
     arma::uword n = data.n_cols;
     arma::uword d = data.n_rows;
 
-    KMedoids kmed(k, "BanditPAM", verbosity);
+    KMedoids kmed(k, "BanditPAM", verbosity, max_iter, log_file_name);
     kmed.fit(data, loss);
 
     if (verbosity > 0) {
