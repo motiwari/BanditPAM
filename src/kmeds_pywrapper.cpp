8914b98c6f157777220785e0ed91081d4dbd8972
Thomas Cheung <thomascheung@Thomass-MacBook-Pro.local>
2021-06-10 16:10:28 -0400

    Update the README document by adding parameter k for number of medoids. Fixes #48

--- src/kmeds_pywrapper.cpp
+++ src/kmeds_pywrapper.cpp
@@ -35,9 +35,11 @@ public:
    *
    * @param inputData Input data to find the medoids of
    * @param loss The loss function used during medoid computation
+   * @param k The number of medoids to compute
    * @param logFilename The name of the outputted log file
    */
-  void fitPython(py::array_t<double> inputData, std::string loss, std::string logFilename) {
+  void fitPython(py::array_t<double> inputData, std::string loss, int k, std::string logFilename) {
+    KMedoids::setNMedoids(k);
     KMedoids::setLogFilename(logFilename);
     KMedoids::fit(carma::arr_to_mat<double>(inputData), loss);
   }
