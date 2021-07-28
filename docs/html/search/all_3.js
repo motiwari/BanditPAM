64ab412eb3070b0f51555f9ce5d1dcf4d080312c
dhaval737 <dhaval737@gmail.com>
2021-07-27 22:42:55 -0700

    Changing all the instances of kmeds/KMeds to kmedoids/KMedoids

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['data',['data',['../classKMedoids.html#afca7201861fc3c605ba25e059cd58764',1,'KMedoids']]]
+  ['data_15',['data',['../classKMedoids.html#afca7201861fc3c605ba25e059cd58764',1,'KMedoids']]]
 ];
