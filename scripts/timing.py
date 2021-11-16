9a819fb5bd8c73260d8f86b355abfaf319a32896
motiwari <mohittiwarinyc+github@gmail.com>
2021-11-16 11:45:03 -0800

    Removing example.svg

--- scripts/timing.py
+++ scripts/timing.py
@@ -10,7 +10,7 @@ import sys
 #banditpam.set_num_threads(int(sys.argv[1]))
 #print(banditpam.sum_thread_ids())
 
-X = np.loadtxt('data/MNIST-10k.csv')
+X = np.loadtxt('data/MNIST-1k.csv')
 kmed = banditpam.KMedoids(n_medoids = 5, algorithm = "BanditPAM")
 start = time.time()
 kmed.fit(X, "L2", "gmm_log")
