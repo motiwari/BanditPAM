5808f37939aea077bbce3fb9c5d8eb1f12470042
Ubuntu <ubuntu@ip-172-31-0-161.us-west-1.compute.internal>
2021-11-18 18:19:31 +0000

    Improving compiler check significantly, appears to work on linux now

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ import setuptools
 import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
+import distutils.sysconfig
 
 __version__ = "1.0.0b"
 
@@ -51,39 +52,7 @@ def compiler_check():
     python -- even if the user specifies another one! -- for some of the
     compilation process
     """
-    def convert_bin_to_bytestring(bin_file):
-        MAX_BYTES = 45
-        str_ = b''
-        tmp = bin_file.read(MAX_BYTES) # can only read 45 bytes at a time
-        while tmp:
-            str_ += tmp
-            tmp = bin_file.read(MAX_BYTES)
-        return str_
-
-    with open('tmp_output.txt', 'wb') as fout, \
-        open('tmp_error.txt', 'wb') as ferr:
-        # Spawn a python3 process, then kill it
-        process = subprocess.Popen(['python3'], stdout=fout, stderr=ferr)
-        time.sleep(1) # Need to wait for python3 to produce output
-        process.send_signal(signal.SIGKILL)
-
-    with open('tmp_output.txt', 'rb') as fout, open('tmp_error.txt', 'rb') as ferr:
-        error = convert_bin_to_bytestring(ferr)
-        if b'Apple' in error:
-            raise Exception("Error: you're using a version of python compiled with Apple Clang, \
-                which does not support OpenMP. Please use Anaconda Python or download LLVM Clang \
-                and build python.")
-        elif b'Clang' in error:
-            compiler = 'clang'
-        elif b'comamnd not found' in error:
-            raise Exception("Please install python3")
-        else:
-            compiler = 'gcc'
-
-    os.remove('tmp_output.txt')
-    os.remove('tmp_error.txt')
-    return compiler
-
+    return 'clang' if 'clang' in distutils.sysconfig.get_config_vars()['CC'] else 'gcc'
 
 def has_flag(compiler, flagname):
     """
