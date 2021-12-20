17cd8de6f8cfd78d3d661b707c44a76614cae1b8
motiwari <mohittiwarinyc+github@gmail.com>
2021-12-20 15:07:58 -0800

    Misnamed file

--- scripts/colab_install_armadillo.sh
+++ scripts/colab_install_armadillo.sh
@@ -3,7 +3,7 @@
 # Unzip the files to their appropriate places
 
 cp /content/BanditPAM/scripts/colab_files/usr-include-armadillo /usr/include/armadillo
-cp /content/BanditPAM/scripts/colab_files/usr-lib-x86_64-linux-gnu-pkgconfig-armadillo /usr/lib/x86_64-linux-gnu/pkgconfig/armadillo.pc
+cp /content/BanditPAM/scripts/colab_files/usr-lib-x86_64-linux-gnu-pkgconfig-armadillo.pc /usr/lib/x86_64-linux-gnu/pkgconfig/armadillo.pc
 tar -zxvf /content/BanditPAM/scripts/colab_files/usr-include-armadillo_bits.tar.gz /usr/include
 tar -zxvf /content/BanditPAM/scripts/colab_files/usr-lib-x86_64-linux-gnu.tar.gz /usr/lib/x86_64-linux-gnu/
 mkdir -p /usr/share/Armadillo
