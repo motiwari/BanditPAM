518d3d3dd3b5173af5c6853af8a0a603ed51007d
motiwari <mohittiwarinyc+github@gmail.com>
2021-12-20 13:18:21 -0800

    Using absolute paths

--- scripts/colab_setup.sh
+++ scripts/colab_setup.sh
@@ -4,16 +4,18 @@ apt-get update
 apt install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev 
 
 # Should be in /content/
+cd /content
 curl -XGET https://codeload.github.com/Kitware/CMake/tar.gz/refs/tags/v3.22.1 > cmake-v3.22.1.tar.gz
 tar -xvzf cmake-v3.22.1.tar.gz
-cd CMake-3.22.1 && mkdir build && cd build && cmake .. && make && make install
+cd /content/CMake-3.22.1 && mkdir build && cd build && cmake .. && make && make install
 
+cd /content
 git clone https://github.com/ThrunGroup/BanditPAM.git
-cd BanditPAM
+cd /content/BanditPAM
 git submodule update --init --recursive
-cd BanditPAM/headers/carma
+cd /content/BanditPAM/headers/carma
 mkdir build && cd build && cmake .. && make
-cd BanditPAM/headers/carma/extern/armadillo-code
+cd /content/BanditPAM/headers/carma/extern/armadillo-code
 mkdir build && cd build && cmake .. && make && make install
 
-pip install BanditPAM
+pip install /content/BanditPAM
