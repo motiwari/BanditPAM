9daab7c95dddae9c971ff0f8178d80391102e8a9
motiwari <mohittiwarinyc+github@gmail.com>
2021-12-22 16:39:52 -0800

    Fixing indentation levels in headers/algorithms

--- headers/algorithms/fastpam1.hpp
+++ headers/algorithms/fastpam1.hpp
@@ -20,45 +20,45 @@ namespace km {
  */
 class FastPAM1 : public km::KMedoids {
  public:
-    /*! \brief Runs FastPAM1 algorithm.
-    *
-    *  Run the FastPAM1 algorithm to identify a dataset's medoids.
-    *
-    *  @param input_data Input data to cluster
-    */
-    void fit_fastpam1(const arma::mat& input_data);
+  /*! \brief Runs FastPAM1 algorithm.
+  *
+  *  Run the FastPAM1 algorithm to identify a dataset's medoids.
+  *
+  *  @param input_data Input data to cluster
+  */
+  void fit_fastpam1(const arma::mat& input_data);
 
-    /*! \brief Build step for the FastPAM1 algorithm
-    *
-    *  Runs build step for the FastPAM1 algorithm. Loops over all datapoint and
-    *  checks its distance from every other datapoint in the dataset, then checks if
-    *  the total cost is less than that of the medoid (if a medoid exists yet).
-    *
-    *  @param data Transposed input data to cluster
-    *  @param medoid_indices Uninitialized array of medoids that is modified in place
-    *  as medoids are identified
-    */
-    void build_fastpam1(
-      const arma::mat& data,
-      arma::urowvec* medoid_indices);
+  /*! \brief Build step for the FastPAM1 algorithm
+  *
+  *  Runs build step for the FastPAM1 algorithm. Loops over all datapoint and
+  *  checks its distance from every other datapoint in the dataset, then checks if
+  *  the total cost is less than that of the medoid (if a medoid exists yet).
+  *
+  *  @param data Transposed input data to cluster
+  *  @param medoid_indices Uninitialized array of medoids that is modified in place
+  *  as medoids are identified
+  */
+  void build_fastpam1(
+    const arma::mat& data,
+    arma::urowvec* medoid_indices);
 
-    /*! \brief Swap step for the FastPAM1 algorithm
-    *
-    *  Runs swap step for the FastPAM1 algorithm. Loops over all datapoint and
-    *  compute the loss change when a medoid is replaced by the datapoint. The
-    *  loss change is stored in an array of size n_medoids and the update is
-    *  based on an if conditional outside of the loop. The best medoid is chosen
-    *  according to the best loss change.
-    *
-    *  @param data Transposed input data to cluster
-    *  @param medoid_indices Array of medoid indices created from the build step
-    *  that is modified in place as better medoids are identified
-    *  @param assignments Uninitialized array of indices corresponding to each
-    *  datapoint assigned the index of the medoid it is closest to
-    */
-    void swap_fastpam1(const arma::mat& data,
-      arma::urowvec* medoid_indices,
-      arma::urowvec* assignments);
+  /*! \brief Swap step for the FastPAM1 algorithm
+  *
+  *  Runs swap step for the FastPAM1 algorithm. Loops over all datapoint and
+  *  compute the loss change when a medoid is replaced by the datapoint. The
+  *  loss change is stored in an array of size n_medoids and the update is
+  *  based on an if conditional outside of the loop. The best medoid is chosen
+  *  according to the best loss change.
+  *
+  *  @param data Transposed input data to cluster
+  *  @param medoid_indices Array of medoid indices created from the build step
+  *  that is modified in place as better medoids are identified
+  *  @param assignments Uninitialized array of indices corresponding to each
+  *  datapoint assigned the index of the medoid it is closest to
+  */
+  void swap_fastpam1(const arma::mat& data,
+    arma::urowvec* medoid_indices,
+    arma::urowvec* assignments);
 };
 }  // namespace km
 #endif  // HEADERS_ALGORITHMS_FASTPAM1_HPP_
--- headers/algorithms/pam.hpp
+++ headers/algorithms/pam.hpp
@@ -21,44 +21,44 @@ namespace km {
  */
 class PAM : public km::KMedoids {
  public:
-    /*! \brief Runs PAM algorithm.
-    *
-    *  Run the PAM algorithm to identify a dataset's medoids.
-    *
-    *  @param input_data Input data to cluster
-    */
-    void fit_pam(const arma::mat& inputData);
+  /*! \brief Runs PAM algorithm.
+  *
+  *  Run the PAM algorithm to identify a dataset's medoids.
+  *
+  *  @param input_data Input data to cluster
+  */
+  void fit_pam(const arma::mat& inputData);
 
-    /*! \brief Build step for the PAM algorithm
-    *
-    *  Runs build step for the PAM algorithm. Loops over all datapoint and
-    *  checks its distance from every other datapoint in the dataset, then checks if
-    *  the total cost is less than that of the medoid (if a medoid exists yet).
-    *
-    *  @param data Transposed input data to cluster
-    *  @param medoid_indices Uninitialized array of medoids that is modified in place
-    *  as medoids are identified
-    */
-    void build_pam(
-      const arma::mat& data,
-      arma::urowvec* medoidIndices);
+  /*! \brief Build step for the PAM algorithm
+  *
+  *  Runs build step for the PAM algorithm. Loops over all datapoint and
+  *  checks its distance from every other datapoint in the dataset, then checks if
+  *  the total cost is less than that of the medoid (if a medoid exists yet).
+  *
+  *  @param data Transposed input data to cluster
+  *  @param medoid_indices Uninitialized array of medoids that is modified in place
+  *  as medoids are identified
+  */
+  void build_pam(
+    const arma::mat& data,
+    arma::urowvec* medoidIndices);
 
-    /*! \brief Swap step for the PAM algorithm
-    *
-    *  Runs build step for the PAM algorithm. Loops over all datapoint and
-    *  checks its distance from every other datapoint in the dataset, then checks if
-    *  the total cost is less than that of the medoid.
-    *
-    *  @param data Transposed input data to find the medoids of
-    *  @param medoid_indices Array of medoid indices created from the build step
-    *  that is modified in place as better medoids are identified
-    *  @param assignments Uninitialized array of indices corresponding to each
-    *  datapoint assigned the index of the medoid it is closest to
-    */
-    void swap_pam(
-      const arma::mat& data,
-      arma::urowvec* medoidIndices,
-      arma::urowvec* assignments);
+  /*! \brief Swap step for the PAM algorithm
+  *
+  *  Runs build step for the PAM algorithm. Loops over all datapoint and
+  *  checks its distance from every other datapoint in the dataset, then checks if
+  *  the total cost is less than that of the medoid.
+  *
+  *  @param data Transposed input data to find the medoids of
+  *  @param medoid_indices Array of medoid indices created from the build step
+  *  that is modified in place as better medoids are identified
+  *  @param assignments Uninitialized array of indices corresponding to each
+  *  datapoint assigned the index of the medoid it is closest to
+  */
+  void swap_pam(
+    const arma::mat& data,
+    arma::urowvec* medoidIndices,
+    arma::urowvec* assignments);
 };
 }  // namespace km
 #endif  // HEADERS_ALGORITHMS_PAM_HPP_
