b9fb2707e6763f7ce2d1709b31ed923c95ec0bcb
motiwari <mohittiwarinyc+github@gmail.com>
2021-12-27 18:48:43 -0800

    Adding shell=True, html extra path

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -61,8 +61,8 @@ html_static_path = ['_static']
 # -- Bridge with Doxygen -------------------------------------------------
 
 # TODO(@motiwari): Unsafe subprocess calls, hacky copy-over of index.html
-subprocess.call('cd .. ; doxygen Doxyfile') 
+subprocess.call('cd .. ; doxygen Doxyfile', shell=True) 
 readme_file = glob.glob("html/*_README.html")[0]
 subprocess.call('cp {} html/index.html'.format(readme_file), shell=True)
 
-html_extra_path = ['../html']
+html_extra_path = ['../html', 'html']
