c58c2b0392edcdeff8b3955b67c4dc0c4090994e
motiwari <mohittiwarinyc+github@gmail.com>
2022-01-14 08:23:44 -0800

    no-op style change

--- src/algorithms/fastpam1.cpp
+++ src/algorithms/fastpam1.cpp
@@ -2,12 +2,13 @@
  * @file fastpam1.cpp
  * @date 2021-08-03
  *
- * Contains the primary C++ implementation of the FastPAM1 code follows
+ * This file contains the primary C++ implementation of the FastPAM1 code follows
  * from the paper: Erich Schubert and Peter J. Rousseeuw: Faster k-Medoids Clustering:
  * Improving the PAM, CLARA, and CLARANS Algorithms. (https://arxiv.org/pdf/1810.05691.pdf).
  * The original PAM papers are:
  * 1) Leonard Kaufman and Peter J. Rousseeuw: Clustering by means of medoids.
  * 2) Leonard Kaufman and Peter J. Rousseeuw: Partitioning around medoids (program pam).
+ *
  */
 
 #include "fastpam1.hpp"
@@ -46,18 +47,16 @@ void FastPAM1::buildFastPAM1(
   arma::frowvec bestDistances(N);
   bestDistances.fill(std::numeric_limits<float>::infinity());
   arma::frowvec sigma(N);
-  float total = 0;
-  float minDistance = std::numeric_limits<float>::infinity();
-  int best = 0;
-  float cost = 0;
-  // TODO (@motiwari): pragma omp parallel for?
   for (size_t k = 0; k < nMedoids; k++) {
-    minDistance = std::numeric_limits<float>::infinity();
-    best = 0;
+    float minDistance = std::numeric_limits<float>::infinity();
+    int best = 0;
+    // fixes a base datapoint
     for (size_t i = 0; i < data.n_cols; i++) {
-      total = 0;
+      float total = 0;
       for (size_t j = 0; j < data.n_cols; j++) {
-        cost = (this->*lossFn)(data, i, j);
+        // computes distance between base and all other points
+        float cost = (this->*lossFn)(data, i, j);
+        // compares this with the cached best distance
         if (bestDistances(j) < cost) {
           cost = bestDistances(j);
         }
@@ -71,10 +70,8 @@ void FastPAM1::buildFastPAM1(
     (*medoidIndices)(k) = best;
 
     // update the medoid assignment and best_distance for this datapoint
-    float cost = 0;
-    #pragma omp parallel for
     for (size_t l = 0; l < N; l++) {
-      cost = (this->*lossFn)(data, l, (*medoidIndices)(k));
+      float cost = (this->*lossFn)(data, l, (*medoidIndices)(k));
       if (cost < bestDistances(l)) {
         bestDistances(l) = cost;
       }
@@ -90,16 +87,12 @@ void FastPAM1::swapFastPAM1(
   float bestChange = 0;
   float minDistance = std::numeric_limits<float>::infinity();
   size_t best = 0;
-  size_t medoidToSwap = 0;
+  size_t medoid_to_swap = 0;
   size_t N = data.n_cols;
   arma::fmat sigma(nMedoids, N, arma::fill::zeros);
   arma::frowvec bestDistances(N);
   arma::frowvec secondBestDistances(N);
   arma::frowvec deltaTD(nMedoids, arma::fill::zeros);
-  bool swapPerformed = true;
-  float di = 0;
-  float dij = 0;
-  size_t iter = 0;
 
   // calculate quantities needed for swap, bestDistances and sigma
   KMedoids::calcBestDistancesSwap(
@@ -107,17 +100,15 @@ void FastPAM1::swapFastPAM1(
     medoidIndices,
     &bestDistances,
     &secondBestDistances,
-    assignments,
-    swapPerformed);
+    assignments);
 
-  while (swapPerformed && iter < maxIter) {
-    iter++;
-    for (size_t i = 0; i < data.n_cols; i++) {
-      di = bestDistances(i);
-      // compute loss change for making i a medoid
-      deltaTD.fill(-di);
-      for (size_t j = 0; j < data.n_cols; j++) {
-        dij = (this->*lossFn)(data, i, j);
+  for (size_t i = 0; i < data.n_cols; i++) {
+    float di = bestDistances(i);
+    // compute loss change for making i a medoid
+    deltaTD.fill(-di);
+    for (size_t j = 0; j < data.n_cols; j++) {
+      if (j != i) {
+        float dij = (this->*lossFn)(data, i, j);
         // update loss change for the current
         if (dij < secondBestDistances(j)) {
           deltaTD.at((*assignments)(j)) += (dij - bestDistances(j));
@@ -133,31 +124,23 @@ void FastPAM1::swapFastPAM1(
           deltaTD.at((*assignments)(j)) -= (dij -  bestDistances(j));
         }
       }
-      
-      // choose the best medoid-to-swap
-      arma::uword newMedoid = deltaTD.index_min();
-      // if the loss change is better than the best loss change,
-      // update the best index identified so far
-      if (deltaTD.min() < bestChange) {
-        bestChange = deltaTD.min();
-        best = i;
-        medoidToSwap = newMedoid;
-      }
     }
-    // update the loss and medoid if the loss is improved
-    if (bestChange < -precision) {
-      (*medoidIndices)(medoidToSwap) = best;
-    } else {
-      swapPerformed = false;
+    // choose the best medoid-to-swap
+    arma::uword minMedoid = deltaTD.index_min();
+    // if the loss change is better than the best loss change,
+    // update the best index identified so far
+    if (deltaTD.min() < bestChange) {
+      bestChange = deltaTD.min();
+      best = i;
+      medoid_to_swap = minMedoid;
     }
-
-    calcBestDistancesSwap(
-      data,
-      medoidIndices,
-      &bestDistances,
-      &secondBestDistances,
-      assignments,
-      swapPerformed);
+  }
+  // update the loss and medoid if the loss is improved
+  if (bestChange < 0) {
+    minDistance = arma::sum(bestDistances) + bestChange;
+    (*medoidIndices)(medoid_to_swap) = best;
+  } else {
+    minDistance = arma::sum(bestDistances);
   }
 }
 }  // namespace km
