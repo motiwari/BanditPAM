e41c443757aaf7370791fb076af27ed8bdafdca8
motiwari <mohittiwarinyc+github@gmail.com>
2022-02-11 18:07:04 -0800

    Verified that running with dist_mat on MNIST10k yields roughly 1/3 lower runtime

--- scripts/timing_dist_mat.py
+++ scripts/timing_dist_mat.py
@@ -4,13 +4,16 @@
 import time
 import numpy as np
 import banditpam
+from sklearn.metrics.pairwise import euclidean_distances
+
 
 # banditpam.set_num_threads(1)
 
 X = np.loadtxt('data/MNIST_10k.csv')
 kmed = banditpam.KMedoids(n_medoids=5, algorithm="BanditPAM")
 start = time.time()
-kmed.fit(X, "L2")
+diss = euclidean_distances(X) # compute distance matrix every time, to be fair to BanditPAM.
+kmed.fit(X, "L2", dist_mat = diss)
 print(time.time() - start, "seconds")
 print("Number of SWAP steps:", kmed.steps)
 print(kmed.medoids)
