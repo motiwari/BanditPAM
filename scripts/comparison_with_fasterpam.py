66d7aa4ca2b1e2ce15f569d0ff48a55d43cd7d14
motiwari <mohittiwarinyc+github@gmail.com>
2022-02-28 15:29:42 -0800

    Stashing changes for comparison script

--- scripts/comparison_with_fasterpam.py
+++ scripts/comparison_with_fasterpam.py
@@ -32,30 +32,39 @@ def benchmark(data, f, n=1):
     print("{:16s} min={:-10.2f} mean={:-10.2f} ±{:-.2f}".format(k, min, avg, ste))
 
 def run_fasterpam(data, seed):
+  diss = euclidean_distances(data)
   start = time.time()
-  diss = euclidean_distances(data) # compute distance matrix every time, to be fair to BanditPAM.
-  mid = time.time()
-  r = fasterpam(diss, 100, random_state=seed, n_cpu=1)
+  r = fasterpam(diss, 5, random_state=seed, n_cpu=-1)
   end = time.time()
   meds, lbl = data[r.medoids], r.labels
   verified_loss = np.sqrt(((data - meds[lbl])**2).sum(axis=1)).sum()
-  return {"time (ms)": (end-start)*1000, "verified loss": verified_loss, "reported loss": r.loss, "opt. time (ms)": (end-mid)*1000, "mat. time (ms)": (mid-start)*1000 }  
+  return {
+    "time (ms)": (end-start)*1000, 
+    "verified loss": verified_loss, 
+    "reported loss": r.loss, 
+    "mat. time (ms)": (end-start)*1000,
+    }  
 
 def run_bandit(data, seed):
-  km = banditpam.KMedoids(100)
+  diss = euclidean_distances(data)
+  km = banditpam.KMedoids(5)
   km.seed = seed
   start = time.time()
-  km.fit(data, "L2")
+  km.fit(data, "L2", dist_mat = diss)
   end = time.time()
   meds, lbl = data[km.medoids], km.labels
   #del km # try to free memory
   verified_loss = np.sqrt(((data - meds[lbl])**2).sum(axis=1)).sum()
-  return {"time (ms)": (end-start)*1000, "verified loss": verified_loss, "reported loss": km.average_loss }  
+  return {
+    "time (ms)": (end-start)*1000,
+    "verified loss": verified_loss,
+    "reported loss": km.average_loss,
+    }  
 
 
 if __name__ == "__main__":
     X, _ = fetch_openml('mnist_784', version=1, return_X_y=True, as_frame=False)
-    X = X[:15000] # at 20k, colab will timeout for BanditPAM
+    X = X[:20000] # at 20k, colab will timeout for BanditPAM
     print(X.shape, type(X))
 
     benchmark(X, run_fasterpam)
