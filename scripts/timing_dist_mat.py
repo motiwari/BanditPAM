cc40fd94c862655275e0c3845e783c2f3e07bc3d
motiwari <mohittiwarinyc+github@gmail.com>
2022-12-23 12:06:05 -0800

    parallelization and cache stats tests passing in cache_measurements.py

--- scripts/timing_dist_mat.py
+++ scripts/timing_dist_mat.py
@@ -5,11 +5,12 @@ import time
 import numpy as np
 import banditpam
 from sklearn.metrics.pairwise import euclidean_distances
+import os
 
 
 # banditpam.set_num_threads(1)
 
-X = np.loadtxt("data/MNIST_10k.csv")
+X = np.loadtxt(os.path.join("data", "MNIST_10k.csv"))
 kmed = banditpam.KMedoids(n_medoids=5, algorithm="BanditPAM")
 start = time.time()
 diss = euclidean_distances(X)
