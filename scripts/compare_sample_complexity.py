e71571ee4207bb48a5c81009772ea302b79dcb7e
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-09 10:34:48 -0800

    BanditPAM sample complexity and runtime seems better than BanditPAMv_3, but still scales with k. Might be due to distribution of confidence intervals

--- scripts/compare_sample_complexity.py
+++ scripts/compare_sample_complexity.py
@@ -1,16 +1,30 @@
 import banditpam
 import numpy as np
 
-def main():
-    banditpam.set_num_threads(1)
-    X = np.loadtxt("data/MNIST_1k.csv")
-    kmed = banditpam.KMedoids(n_medoids=5, algorithm="BanditPAM", use_cache=False, use_perm=False, parallelize=False)
-    kmed.fit(X, "L2")
+def print_results(kmed):
+    print("-----Results-----")
+    print(kmed.algorithm)
+    print(kmed.medoids)
+    print("Loss:", kmed.average_loss)
     print("Misc complexity:", kmed.misc_distance_computations)
     print("Build complexity:", kmed.build_distance_computations)
     print("Swap complexity:", kmed.swap_distance_computations)
+    print("Number of Swaps", kmed.steps)
     print("Total complexity:", kmed.getDistanceComputations(True))
     print("Total complexity:", kmed.getDistanceComputations(False))
+
+
+def main():
+    banditpam.set_num_threads(1)
+    X = np.loadtxt("data/MNIST_1k.csv")
+
+    kmed = banditpam.KMedoids(n_medoids=10, algorithm="BanditPAM", use_cache=False, use_perm=False, parallelize=False)
+    kmed.fit(X, "L2")
+    print_results(kmed)
+
+    kmed = banditpam.KMedoids(n_medoids=10, algorithm="BanditPAM_v3", use_cache=False, use_perm=False, parallelize=False)
+    kmed.fit(X, "L2")
+    print_results(kmed)
 
 
 
