faccdf3b713330a1c253624e3c16f55ab95c6abe
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-17 16:53:22 -0800

    Changing version to 4.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.build_ext import build_ext
 import distutils.sysconfig
 import distutils.spawn
 
-__version__ = "4.0.1a"
+__version__ = "4.0.2"
 
 # TODO(@motiwari): Move this to a separate file
 GHA = "GITHUB_ACTIONS"
