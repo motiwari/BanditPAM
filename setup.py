23191aed962162d4adcf072fd1cd448d0267e882
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-18 15:24:45 -0800

    Adding back -Xpreprocessor -fopenmp

--- setup.py
+++ setup.py
@@ -374,7 +374,7 @@ class BuildExt(build_ext):
         opts.append(cpp_flag(self.compiler))
         opts.append("-O3")
         if sys.platform == "darwin" and os.environ.get(GHA, False):
-            opts.append("-fopenmp")
+            opts.append("-Xpreprocessor -fopenmp")
         else:
             opts.append("-fopenmp")
 
