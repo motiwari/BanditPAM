de69021732449c46778ce7c3e593483a84b0dfa5
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-17 18:03:36 -0800

    Removing libreadline-gplv2-dev from some more files

--- scripts/colab_files/colab_install_armadillo.sh
+++ scripts/colab_files/colab_install_armadillo.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies on Google Colab machine
 
-apt install -y build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+apt install -y build-essential checkinstall libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
 # Copies and unzips the files to their appropriate places
 
