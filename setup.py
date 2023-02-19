e0b775d8e1ae6eed853df65ea0b51e787285a72a
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-18 20:44:37 -0800

    Uploading to testpypi dry run

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.build_ext import build_ext
 import distutils.sysconfig
 import distutils.spawn
 
-__version__ = "4.0.0a"
+__version__ = "4.0.0a0"
 
 # TODO(@motiwari): Move this to a separate file
 GHA = "GITHUB_ACTIONS"
