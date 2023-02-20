ed56081301e91946bd1fc401c8dcc5ee69a9847a
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-20 13:35:50 -0800

    Trying to compile with homebrew gcc - 3

--- scripts/docker/cibw_before_all_macos.sh
+++ scripts/docker/cibw_before_all_macos.sh
@@ -1,3 +1,6 @@
+export PATH="/usr/local/Cellar/gcc/12.2.0/bin:$PATH"
+export CC="/usr/local/Cellar/gcc/12.2.0/bin/gcc-12"
+export CXX="/usr/local/Cellar/gcc/12.2.0/bin/g++-12"
 cd ~
 git clone https://gitlab.com/conradsnicta/armadillo-code.git
 git clone https://github.com/RUrlus/carma.git --recursive # Do we need this?
@@ -8,4 +11,4 @@ cd ~/carma
 mkdir -p build
 cd build
 cmake -DCARMA_INSTALL_LIB=ON ..
-sudo cmake --build . --config Release --target install
+sudo CC="/usr/local/Cellar/gcc/12.2.0/bin/gcc-12" CXX="/usr/local/Cellar/gcc/12.2.0/bin/g++-12" cmake --build . --config Release --target install
