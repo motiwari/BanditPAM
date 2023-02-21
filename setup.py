fb41c4ce5da877a9ef0dde980a7a99f28ccbbd7e
motiwari <mohittiwarinyc+github@gmail.com>
2023-02-20 16:09:33 -0800

    Trying without a brew update and just adding libomp dirs to DYLD_LIBRARY_PATH for delocate

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.build_ext import build_ext
 import distutils.sysconfig
 import distutils.spawn
 
-__version__ = "4.0.1a0"
+__version__ = "4.0.2a0"
 
 # TODO(@motiwari): Move this to a separate file
 GHA = "GITHUB_ACTIONS"
