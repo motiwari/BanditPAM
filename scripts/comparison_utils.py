62932e6c8e560a5ad279c811a7a0902c21476c58
Luke Donghyun Lee <lukeleeai@Lukes-MacBook-Pro-26.local>
2023-07-06 00:20:58 +0900

    Added more stats to print out in comparison_utils.py

--- scripts/comparison_utils.py
+++ scripts/comparison_utils.py
@@ -2,7 +2,9 @@ from banditpam import KMedoids
 
 
 def print_results(kmed: KMedoids, runtime: float):
-    complexity_with_caching = kmed.getDistanceComputations(True) - kmed.cache_hits
+    complexity_with_caching = (
+        kmed.getDistanceComputations(True) - kmed.cache_hits
+    )
     print("-----Results-----")
     print("Algorithm:", kmed.algorithm)
     print("Final Medoids:", kmed.medoids)
@@ -14,6 +16,10 @@ def print_results(kmed: KMedoids, runtime: float):
     print("Cache Writes: {:,}".format(kmed.cache_writes))
     print("Cache Hits: {:,}".format(kmed.cache_hits))
     print("Cache Misses: {:,}".format(kmed.cache_misses))
+    print(
+        "Average Swap Sample Complexity:",
+        f"{kmed.swap_distance_computations / kmed.steps:,}",
+    )
     print(
         "Total complexity (without misc):",
         f"{kmed.getDistanceComputations(False):,}",
@@ -27,3 +33,4 @@ def print_results(kmed: KMedoids, runtime: float):
         f"{complexity_with_caching:,}",
     )
     print("Total runtime:", runtime)
+    print("Runtime per swap:", runtime / kmed.steps)
