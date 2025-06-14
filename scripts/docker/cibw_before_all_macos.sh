83e894d99317829a59c4d3127e89ed1c451bacdd
motiwari <mohittiwarinyc+github@gmail.com>
2025-06-14 13:35:26 -0700

    Fixing GHA again

--- scripts/docker/cibw_before_all_macos.sh
+++ scripts/docker/cibw_before_all_macos.sh
@@ -2,10 +2,10 @@ cd ~
 git clone https://gitlab.com/conradsnicta/armadillo-code.git
 git clone https://github.com/RUrlus/carma.git --recursive # Do we need this?
 cd ~/armadillo-code
-cmake .
-make install
+sudo cmake .
+sudo make install
 cd ~/carma
 mkdir -p build
 cd build
-cmake -DCARMA_INSTALL_LIB=ON ..
+sudo cmake -DCARMA_INSTALL_LIB=ON ..
 sudo cmake --build . --config Release --target install
