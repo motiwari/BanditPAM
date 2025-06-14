8ba3790f733c3a9a6d7618f219a63ccf10ab6fa0
motiwari <mohittiwarinyc+github@gmail.com>
2025-06-14 13:35:26 -0700

    Adding quotes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.build_ext import build_ext
 import distutils.sysconfig
 import distutils.spawn
 
-__version__ = "5.0.1"
+__version__ = "6.0.1"
 
 # TODO(@motiwari): Move this to a separate file
 GHA = "GITHUB_ACTIONS"
